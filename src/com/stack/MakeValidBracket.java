4e9d6e4ca05d295c6ef88ee7a047b05cf9810100
puja1245 <pujakumari1245@gmail.com>
2023-03-10 01:43:41 +0530

    hbj

--- src/com/stack/MakeValidBracket.java
+++ src/com/stack/MakeValidBracket.java
@@ -6,26 +6,26 @@ import java.util.HashMap;
 import java.util.Stack;
 
 public class MakeValidBracket {
-    public String makeValidBracket(String input ){
+    public String makeValidBracket(String input) {
         StringBuilder validString = new StringBuilder(input);
         String[] inputArray = input.split("");
         Stack<Integer> stack = new Stack<>();
-        for(int i =0 ; i< inputArray.length ; i++){
+        for (int i = 0; i < inputArray.length; i++) {
             String temp = inputArray[i];
-            if(temp.equals("(")){
+            if (temp.equals("(")) {
                 stack.push(i);
-            } else if (temp.equals(")")){
-                if(!stack.isEmpty() ){
+            } else if (temp.equals(")")) {
+                if (!stack.isEmpty()) {
                     stack.pop();
-                }else {
-                    validString.setCharAt(i,'*');
+                } else {
+                    validString.setCharAt(i, '*');
                 }
             }
         }
 
-        if(!stack.isEmpty()){
-            int size  = stack.size();
-            for(int i = 0 ; i< size  ; i++){
+        if (!stack.isEmpty()) {
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
                 validString.setCharAt(stack.pop(), '*');
             }
         }
@@ -33,6 +33,7 @@ public class MakeValidBracket {
         //jwdhwjhdjh
 
 
-        return validString.toString().replace("*","");
+        return validString.toString().replace("*", "");
 
     }
+}
